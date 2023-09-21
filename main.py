ea7887517e3da655bcae926195cef67a598a0ace
Vlad <84966677+Vlad-stackover@users.noreply.github.com>
2023-09-21 12:49:13 +0200

    zadanie 1

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-print('PDW KOTY!')
-rok = int(input('Wprowadź rok urodzenia  '))
-wynik = 2023 - rok
-print('Twój wiek to:', wynik)
+# print('PDW KOTY!')
+# rok = int(input('Wprowadź rok urodzenia  '))
+# wynik = 2023 - rok
+# print('Twój wiek to:', wynik)
+a = int(input('Wprowadź liczbę a'))
+b = int(input('Wprowadź liczbę b'))
+if a > b:
+    print(b-a)
+else:
+    print(a-b)
