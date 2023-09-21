af49023315ea89803a75c7ccdd47c20f1c444c6f
Vlad <84966677+Vlad-stackover@users.noreply.github.com>
2023-09-21 13:10:19 +0200

    v2
    Ciąg binarny

--- main.py
+++ main.py
@@ -2,9 +2,36 @@
 # rok = int(input('Wprowadź rok urodzenia  '))
 # wynik = 2023 - rok
 # print('Twój wiek to:', wynik)
-a = int(input('Wprowadź liczbę a'))
-b = int(input('Wprowadź liczbę b'))
+
+a = int(input('Wprowadź liczbę a: '))
+b = int(input('Wprowadź liczbę b: '))
 if a > b:
     print(b-a)
 else:
     print(a-b)
+
+
+#WYSZUKIWANIE BINARNE
+# l = 0
+# p = 7
+# sr = (l+p)/2
+# szukana = int(input('Wprowadź szukaną liczbę'))
+# tab = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+# pozycja = 0
+
+# l = 0
+# p = 15
+# sr = (l+p)/2
+# while l <= p:
+#     if tab[sr] == szukana:
+#         return sr
+
+#     elif tab[sr] > szukana:
+#         p = sr - 1
+
+#     else:
+#         l = sr + 1
+
+#     sr = (l+p)/2
+
+#     return -1
