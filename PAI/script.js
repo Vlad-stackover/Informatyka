8551a5b5e26e2b68d82b8d18e651cd4062e1a3c0
Vlad-stackover <strixus480@gmail.com>
2023-09-24 19:06:26 +0200

    v2.0

---
+++ PAI/script.js
@@ -0,0 +1,15 @@
+// x = prompt("Podaj liczbe:")
+// var liczba = (x % 7 == 0) ? 'podzielna' : 'niepodzielna'
+// document.write(liczba)
+
+// for(let i=1; i<11; i++){
+//     document.write("Pętla nr...", i, '<hr>')
+// }
+
+tablica = [7,5,4,3,2]
+let suma = 0
+for (let i = 0; i < tablica.length; i++){
+    suma += tablica[i]
+}
+
+document.write(suma)
