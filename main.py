07c97f82bd3781ab287ab788978c320788d374f4
Vlad <84966677+Vlad-stackover@users.noreply.github.com>
2023-09-28 13:11:23 +0200

    wyszukiwanie binarne

--- main.py
+++ main.py
@@ -3,35 +3,37 @@
 # wynik = 2023 - rok
 # print('Twój wiek to:', wynik)
 
-a = int(input('Wprowadź liczbę a: '))
-b = int(input('Wprowadź liczbę b: '))
-if a > b:
-    print(b-a)
-else:
-    print(a-b)
+# a = int(input('Wprowadź liczbę a: '))
+# b = int(input('Wprowadź liczbę b: '))
+# if a > b:
+#     print(b-a)
+# else:
+#     print(a-b)
 
 
 #WYSZUKIWANIE BINARNE
-# l = 0
-# p = 7
-# sr = (l+p)/2
-# szukana = int(input('Wprowadź szukaną liczbę'))
-# tab = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
-# pozycja = 0
 
-# l = 0
-# p = 15
-# sr = (l+p)/2
-# while l <= p:
-#     if tab[sr] == szukana:
-#         return sr
 
-#     elif tab[sr] > szukana:
-#         p = sr - 1
 
-#     else:
-#         l = sr + 1
+szukana = int(input('Wprowadź szukaną liczbę: '))
+tab = [1,2,3,4,5,6,7,8,9,10,11]
+l = tab[0]
+p = tab[-1]
+sr = round((l+p)/2)
+pozycja = 0
 
-#     sr = (l+p)/2
 
-#     return -1
+
+while l <= p:
+    if tab[sr] == szukana:
+        print('Pozycja liczby', szukana, 'w tablicy to', tab[sr])
+
+    if tab[sr] > szukana:
+        p = sr - 1
+
+    else:
+        l = sr + 1
+
+    sr = (l+p)/2
+
+    # return -1
