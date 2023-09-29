c69c50593608351d851484fed1af1888537c55d9
Vlad <strixus480@gmail.com>
2023-09-30 00:33:17 +0200

    list sum

--- main.py
+++ main.py
@@ -15,25 +15,35 @@
 
 
 
-szukana = int(input('Wprowadź szukaną liczbę: '))
-tab = [1,2,3,4,5,6,7,8,9,10,11]
+#szukana = int(input('Wprowadź szukaną liczbę: '))
+tab = [1,2,3,4]
 l = tab[0]
 p = tab[-1]
 sr = round((l+p)/2)
 pozycja = 0
 
+suma = 0
 
+x = 0
+y = 1
+while y <= tab[-2]:
+    suma += tab[x] + tab[y]
+    x += 1
+    y += 1
+    
 
-while l <= p:
-    if tab[sr] == szukana:
-        print('Pozycja liczby', szukana, 'w tablicy to', tab[sr])
+print(suma)
 
-    if tab[sr] > szukana:
-        p = sr - 1
+# while l <= p:
+#     if tab[sr] == szukana:
+#         print('Pozycja liczby', szukana, 'w tablicy to', tab[sr])
 
-    else:
-        l = sr + 1
+#     if tab[sr] > szukana:
+#         p = sr - 1
 
-    sr = (l+p)/2
+#     else:
+#         l = sr + 1
 
-    # return -1
+#     sr = (l+p)/2
+
+#     # return -1
