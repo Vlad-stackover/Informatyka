06e295a911ca615562042cf5d6fe06e152b156b6
Vlad <84966677+Vlad-stackover@users.noreply.github.com>
2024-02-29 11:00:30 +0100

    calkowanie v2

--- informatyka/calkowanieNumeryczne.py
+++ informatyka/calkowanieNumeryczne.py
@@ -1,9 +1,9 @@
 def fun(x):
     return x * x + x + 2
 
-a = int(input('Pierwsza wartość przedziału [a,b] '))
-b = int(input('druga wartość przedziału [a, b]' )) 
-n = int(input('liczba prostokątów ' )) 
+a = int(input('Pierwsza wartość przedziału [a,b]     '))
+b = int(input('druga wartość przedziału [a, b]    ' )) 
+n = int(input('liczba prostokątów     ' )) 
 
 def obliczPole(a,b,n):
     x = (b-a) / n
@@ -11,7 +11,7 @@ def obliczPole(a,b,n):
     srodek = a + (b-a) / (2.0 * n)
 
     for i in range(n):
-        S = S + f(srodek)
+        S = S + fun(srodek)
         srodek += x
         return S * x
 
