923615f69103fb182ca69f850e142381553acd12
Vlad <84966677+Vlad-stackover@users.noreply.github.com>
2024-09-30 11:04:09 +0200

    PAI

--- FORUM/script.js
+++
@@ -1,9 +0,0 @@
-function Sprawdz(form) {
-    if ((form.nick.value || form.topic.value || form.title.value || form.text.value) == '') {
-        alert('Empty field!!!');
-        form.focus();
-        return false;
-    }
-    
-    return true;
-}
